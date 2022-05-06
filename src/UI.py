4429d1b5635f1c2ad440fabb1fd0704b62424f35
Yunseong Ha <gci28267@gmail.com>
2022-05-06 17:20:01 +0900

    예비 한글 입력기 추가 (한글 출력 가능하지만, 입력은 불가능할 때 사용 가능)

--- src/UI.py
+++ src/UI.py
@@ -3,7 +3,7 @@
 from PySide6.QtCore import QMetaObject, QCoreApplication
 # from PySide6.QtGui import
 from PySide6.QtWidgets import (
-    QWidget, QGroupBox, QGridLayout,
+    QHBoxLayout, QWidget, QGroupBox, QGridLayout,
     QCheckBox, QPushButton, QLabel, QPlainTextEdit,
     QSpacerItem, QSizePolicy,
 )
@@ -12,53 +12,56 @@ from PySide6.QtWidgets import (
 class Ui_Main(object):
     def setupUi(self, Main):
         if not Main.objectName():
-            Main.setObjectName(u"Main")
+            Main.setObjectName("Main")
         Main.resize(800, 600)
 
         self.centralwidget = QWidget(Main)
-        self.centralwidget.setObjectName(u"centralwidget")
+        self.centralwidget.setObjectName("centralwidget")
         self.glCent = QGridLayout(self.centralwidget)
-        self.glCent.setObjectName(u"glCent")
+        self.glCent.setObjectName("glCent")
 
 
         self.gbConvert = QGroupBox(self.centralwidget)
-        self.glConvert = QGridLayout(self.gbConvert)
+        self.hlConvert = QHBoxLayout(self.gbConvert)
 
-        self.glConvert.addItem(
-            QSpacerItem(616, 20, QSizePolicy.Expanding, QSizePolicy.Minimum),
-            0, 0, 1, 1
+        self.hlConvert.addItem(
+            QSpacerItem(616, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
         )
 
+        self.chkHangulInput = QCheckBox(self.centralwidget)
+        self.chkHangulInput.setObjectName("chkHangulInput")
+        self.hlConvert.addWidget(self.chkHangulInput)
+
         self.chkAuto = QCheckBox(self.centralwidget)
-        self.chkAuto.setObjectName(u"chkAuto")
+        self.chkAuto.setObjectName("chkAuto")
         self.chkAuto.setChecked(True)
-        self.glConvert.addWidget(self.chkAuto, 0, 1, 1, 1)
+        self.hlConvert.addWidget(self.chkAuto)
 
         self.btnTxtEscape = QPushButton(self.centralwidget)
-        self.btnTxtEscape.setObjectName(u"btnTxtEscape")
-        self.glConvert.addWidget(self.btnTxtEscape, 0, 2, 1, 1)
+        self.btnTxtEscape.setObjectName("btnTxtEscape")
+        self.hlConvert.addWidget(self.btnTxtEscape)
 
         self.btnEscapeTxt = QPushButton(self.centralwidget)
-        self.btnEscapeTxt.setObjectName(u"btnEscapeTxt")
-        self.glConvert.addWidget(self.btnEscapeTxt, 0, 3, 1, 1)
+        self.btnEscapeTxt.setObjectName("btnEscapeTxt")
+        self.hlConvert.addWidget(self.btnEscapeTxt)
 
-        self.glCent.addWidget(self.gbConvert, 0, 0, 1, 1)
+        self.glCent.addWidget(self.gbConvert)
 
 
         self.lbTest = QLabel(self.centralwidget)
-        self.lbTest.setObjectName(u"lbTest")
+        self.lbTest.setObjectName("lbTest")
         self.glCent.addWidget(self.lbTest, 1, 0)
 
         self.pteText = QPlainTextEdit(self.centralwidget)
-        self.pteText.setObjectName(u"pteText")
+        self.pteText.setObjectName("pteText")
         self.glCent.addWidget(self.pteText, 2, 0)
 
         self.lbEscape = QLabel(self.centralwidget)
-        self.lbEscape.setObjectName(u"lbEscape")
+        self.lbEscape.setObjectName("lbEscape")
         self.glCent.addWidget(self.lbEscape, 3, 0)
 
         self.pteEscape = QPlainTextEdit(self.centralwidget)
-        self.pteEscape.setObjectName(u"pteEscape")
+        self.pteEscape.setObjectName("pteEscape")
         self.glCent.addWidget(self.pteEscape, 4, 0)
 
         Main.setCentralWidget(self.centralwidget)
@@ -69,11 +72,12 @@ class Ui_Main(object):
     # setupUi
 
     def retranslateUi(self, Main):
-        Main.setWindowTitle(QCoreApplication.translate("MainWindow", u"\uc720\ub2c8\ucf54\ub4dc \ubcc0\ud658\uae30", None))
-        self.gbConvert.setTitle(QCoreApplication.translate("Main", u"\ubcc0\ud658", None))
-        self.chkAuto.setText(QCoreApplication.translate("Main", u"\uc790\ub3d9 \ubcc0\ud658", None))
-        self.btnTxtEscape.setText(QCoreApplication.translate("Main", u"\ud14d\uc2a4\ud2b8\u2192Escape", None))
-        self.btnEscapeTxt.setText(QCoreApplication.translate("Main", u"Escape\u2192\ud14d\uc2a4\ud2b8", None))
-        self.lbTest.setText(QCoreApplication.translate("Main", u"\uc6d0\ubcf8 \ud14d\uc2a4\ud2b8", None))
-        self.lbEscape.setText(QCoreApplication.translate("Main", u"Unicode Escape", None))
+        Main.setWindowTitle(QCoreApplication.translate("MainWindow", "\uc720\ub2c8\ucf54\ub4dc \ubcc0\ud658\uae30", None))
+        self.gbConvert.setTitle(QCoreApplication.translate("Main", "\ubcc0\ud658", None))
+        self.chkHangulInput.setText(QCoreApplication.translate("Main", "\ud55c\uae00 \uc785\ub825\uae30 \ud65c\uc131\ud654", None))
+        self.chkAuto.setText(QCoreApplication.translate("Main", "\uc790\ub3d9 \ubcc0\ud658", None))
+        self.btnTxtEscape.setText(QCoreApplication.translate("Main", "\ud14d\uc2a4\ud2b8\u2192Escape", None))
+        self.btnEscapeTxt.setText(QCoreApplication.translate("Main", "Escape\u2192\ud14d\uc2a4\ud2b8", None))
+        self.lbTest.setText(QCoreApplication.translate("Main", "\uc6d0\ubcf8 \ud14d\uc2a4\ud2b8", None))
+        self.lbEscape.setText(QCoreApplication.translate("Main", "Unicode Escape", None))
     # retranslateUi
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-from PySide6.QtWidgets import QMainWindow, QApplication
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QMainWindow, QApplication, QStyle
 
 from UI import Ui_Main
+from python_qt_hangul_input import PythonQtHangulInputFilter, HangulIndicator
 
 
 class Main(QMainWindow, Ui_Main):
@@ -12,7 +14,15 @@ class Main(QMainWindow, Ui_Main):
 
         self.__status = 0
         self.__auto = True
+        self.__hangul_input_filter = PythonQtHangulInputFilter()
+        self.__hangul_input_indicator = HangulIndicator(self)
 
+        self.__hangul_input_filter.hangul_status_changed.connect(
+            self.__hangul_input_indicator.set_hangul_status
+        )
+        self.chkHangulInput.toggled.connect(
+            self.__on_hangul_input_enabled_changed
+        )
         self.chkAuto.stateChanged.connect(self.__set_auto)
         self.btnTxtEscape.clicked.connect(lambda: self.__text_escape(True))
         self.btnEscapeTxt.clicked.connect(lambda: self.__escape_text(True))
@@ -40,6 +50,37 @@ class Main(QMainWindow, Ui_Main):
                     .encode('ascii').decode('unicode-escape')
             )
             self.__status = 0
+
+    # Hangul input related
+    def __on_hangul_input_enabled_changed(self, enabled: bool):
+        if enabled:
+            self.__hangul_input_indicator.show()
+            QApplication.instance().installEventFilter(
+                self.__hangul_input_filter
+            )
+        else:
+            self.__hangul_input_filter.reset()
+            self.__hangul_input_indicator.hide()
+            QApplication.instance().removeEventFilter(
+                self.__hangul_input_filter
+            )
+
+    def __move_hangul_indicator(self):
+        indicator_size = self.__hangul_input_indicator.size()
+        rect = QStyle.alignedRect(
+            Qt.LeftToRight, Qt.AlignBottom | Qt.AlignRight,
+            indicator_size, self.geometry()
+        )
+        self.__hangul_input_indicator.setPosition.emit(
+            rect.x(), rect.y() + indicator_size.height()
+        )
+    # End hangul input
+
+    def moveEvent(self, _):
+        self.__move_hangul_indicator()
+
+    def resizeEvent(self, _):
+        self.__move_hangul_indicator()
 
 
 def main():
